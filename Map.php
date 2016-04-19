54c78ed13426c9f55e5de412f1e3f6685cea6665
Tom Duthoit <tomduthoit.td@gmail.com>
2016-04-19 15:04:30 +0200

    correction des erreurs

--- Map.php
+++ Map.php
@@ -7,12 +7,17 @@
 
         //constructeur du plateau de jeux
         function __construct($taille){
-            $this->taille = &taille;
+            $this->taille = $taille;
             $this ->matrice = array(array());
-            for(&i=0;&i<&taille;&i++){
-                for(&j=0;&j < &largeur;&j++){
-                    &this->matrice[&i][&j]=rand(0,1);
+            for($i=0;$i<$taille;$i++){
+                for($j=0;$j < $largeur;$j++){
+                    $this->matrice[$i][$j]=rand(0,1);
                 }
+            }
+        }
+        function afficher(){
+            for ($i= 0;$i <$this->taille;&i++){
+                for($j = 0; $j< $this->taille;j++)
             }
         }
     }
