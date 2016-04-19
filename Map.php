ece0e005525016d6a5089fe90aa1574480ca2e9f
Tom Duthoit <tomduthoit.td@gmail.com>
2016-04-19 14:56:36 +0200

    création du plateau de jeux

---
+++ Map.php
@@ -0,0 +1,18 @@
+<?php
+    class map{
+        //déclaration des variables utilisés
+        private &taille;
+        private &matrice;
+
+
+        //constructeur du plateau de jeux
+        function __construct($taille){
+            $this->taille = &taille;
+            $this ->matrice = array(array());
+            for(&i=0;&i<&taille;&i++){
+                for(&j=0;&j < &largeur;&j++){
+                    &this->matrice[&i][&j]=rand(0,1);
+                }
+            }
+        }
+    }
--- index.php
+++ index.php
@@ -7,6 +7,8 @@
                     <link rel="stylesheet" type="text/css" href="./css/style.css"/>
     </head>
     <body>
+        <?php
         
+        ?>
     </body>
 </html>
