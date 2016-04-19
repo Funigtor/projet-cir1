ffca9c7562cc1f0bbd3ef0fd652e748cd43f7c7e
Aurélien Chevalier <aurelien.chevalier@isen-lille.fr>
2016-04-19 15:38:19 +0200

    Début du style de la page

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
             <title>Lightsout</title>
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+                    <link rel="stylesheet" type="text/css" href="./style.css"/>
     </head>
     <body>
+        <h1>Lights Out</h1>
         <table>
             <tr>
                 <td>
@@ -20,5 +21,8 @@
                 </td>
             </tr>
         </table>
+        <footer>
+            Notre projet CIR 1 <!-- Insérer quelquechose ici -->
+        </footer>
     </body>
 </html>
