fda660a12bb971a3feedde9713ce8d72711cf315
Aurélien Chevalier <funigtor@me.com>
2016-04-22 19:09:57 +0200

    Ajout de la page de victoire

---
+++ victory.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            <title>Lightsout</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link rel="stylesheet" type="text/css" href="./style.css"/>
+    </head>
+    <body>
+        <?php include "header.php" ?>
+        <audio src="sound/victory.ogg" autoplay></audio>
+        <div class="faker"></div>
+        <div class="win">
+            VICTOIRE !
+        </div>
+        <div class="faker"></div>
+        <?php include "footer.php" ?>
+    </body>
+</html>
