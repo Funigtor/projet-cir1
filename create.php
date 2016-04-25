24bf54f9bc13e0fdcbff2d414f6668f8c066562b
Aurélien Chevalier <aurelien.chevalier@isen-lille.fr>
2016-04-25 16:03:24 +0200

    Création d'une matrice 5x5 si la création est appelée avec des arguments incorrects.
    Fixes : #1

--- create.php
+++ create.php
@@ -2,12 +2,12 @@
 require_once "./Map.php";
 session_start();
 
-if (isset($_POST["largeur"]) && isset($_POST["hauteur"])) {
+if (isset($_POST["largeur"]) && isset($_POST["hauteur"]) && $_POST["largeur"] != NULL && $_POST["hauteur"] != NULL) {
     $largeur = $_POST["largeur"];
     $hauteur = $_POST["hauteur"];
     $matrice = new Map($largeur,$hauteur);
-}
-else {
+} else {
+    // 5x5 est la dimension de base si elles ne sont pas indiquées ou incomplètes.
     $matrice = new Map(5,5);
 }
 
